466b0d608cc6b28091a273bfaabba9b19806e517
kk <kk@wd.com>
2026-02-11 09:39:27 +0800

    chore: remove test files

--- create-localpath.lua
+++
@@ -1,5 +0,0 @@
-local pathValue = Instance.new("StringValue")
-pathValue.Name = "LocalPlacePath"
-pathValue.Value = "D:/Projects/TestGame"
-pathValue.Parent = workspace
-return "Created LocalPlacePath: " .. pathValue.Value
--- test-workspace.lua
+++
@@ -1,5 +0,0 @@
-local result = {}
-for _, child in ipairs(workspace:GetChildren()) do
-    table.insert(result, child.Name .. " (" .. child.ClassName .. ")")
-end
-return result
